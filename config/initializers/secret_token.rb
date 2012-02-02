3a184b23730bfb43352ace089b2ff073ab9f8b61
Your Name <fificek@hotmail.com>
2012-02-02 17:54:40 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mylilpage::Application.config.secret_token = 'd12ef0353b7788ee4cef1dae340e22cc624f274afaa6dc829c131646e1683cd4bdb254d8636a26271ffb4553ff425ace771d032b45261c37336b88fe628aecfd'
